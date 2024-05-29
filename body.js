9f49210509aa312ff26b5552c280f0e66ef620a5
Robert <junhojeon010@gmail.com>
2024-05-29 07:39:11 -0600

    Background color to pink

--- body.js
+++ body.js
@@ -1,9 +1,12 @@
-document.body.style.display = "flex";
-document.documentElement.style.height = "100%";
-document.body.style.height = "100%";
-document.body.style.justifyContent = "center";
-document.body.style.alignItems = "center";
-document.body.style.flexDirection = "column";
+
+
+document.body.style.cssText = `
+    display: flex;
+    flexDirection = "column"; 
+    height = 100%;
+    alignItems = "center";
+    justifyContent = "center";`
+
 
 // ----- The Calculator's Skeleton ----- // 
 
@@ -14,6 +17,7 @@ calcShell.style.flexDirection = "column";
 calcShell.style.height = "500px";
 calcShell.style.width = "300px"; 
 calcShell.style.border = "1px solid black";
+calcShell.style.backgroundColor = "#FFEFFE";
 
 document.body.appendChild(calcShell);
 
@@ -23,12 +27,14 @@ let x;
 const resultShell = document.createElement("div");
 resultShell.classList.add("resultShell");
 resultShell.style.display = "flex";
+resultShell.style.flexDirection = "column";
 resultShell.style.height = "75px";
 resultShell.style.width = "240px";
 resultShell.style.marginLeft = "auto";
 resultShell.style.marginRight = "auto";
 resultShell.style.marginTop = "30px";
 resultShell.style.border = "1px solid black"; 
+
 
 calcShell.appendChild(resultShell);
 
@@ -37,5 +43,14 @@ subDisplayShell.classList.add("subDisplayShell");
 subDisplayShell.style.height = "25px";
 subDisplayShell.style.width = "240px";
 subDisplayShell.style.border = "1px solid black";
+subDisplayShell.style.fontSize = "10px";
 
 resultShell.appendChild(subDisplayShell);
+
+const displayShell = document.createElement("div");
+displayShell.classList.add("displayShell");
+displayShell.style.height = "50px";
+displayShell.style.width = "240px";
+displayShell.style.border = "1px solid black";
+
+resultShell.appendChild(displayShell);
