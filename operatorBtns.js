6faf4dcf710cc7d32114c1c2b2e28db91c4c1746
Robert <junhojeon010@gmail.com>
2024-05-30 21:03:03 -0600

    added conditionals to plusBtn event listener to trigger evaluation of digits if the plus button is clicked instead of the equal button

--- operatorBtns.js
+++ operatorBtns.js
@@ -23,6 +23,7 @@ const mapOpBtn = opBtnArr.map((oper) => {
     for (i = 0; i < opBtnArr.length; i++) {
         opBtnArr[i].style.height = "55px";
         opBtnArr[i].style.width = "55px";
+        opBtnArr[i].style.backgroundColor = "#FFFFCC";
         opBtnArr[i].textContent = `${opArr[i]}`;
         operContainer.appendChild(opBtnArr[i]); 
     }
@@ -35,16 +36,25 @@ numShell.appendChild(operContainer);
 let y; 
 
 clrBtn.addEventListener("click", () => {
+    valueDict = {
+        x: '',
+        y: '',
+        op: '',             
+    };
     displayShell.textContent = ''; 
     subDisplayShell.textContent = '';
 });
 
 plusBtn.addEventListener("click", () => {
+    if (valueDict.x != '') {
+        valueDict.y = `${displayShell.textContent}`;
+        operate(valueDict.op, parseFloat(valueDict.x), parseFloat(valueDict.y));
+    } else {
     valueDict.x = `${displayShell.textContent}`
     valueDict.op = `+`;
     displayShell.textContent = '';
     subDisplayShell.textContent = subDisplayShell.textContent + ' + '; 
-    console.log(valueDict);
+    };
 }); 
 
 subBtn.addEventListener("click", () => {
